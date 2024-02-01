6470b89b2fb5975aa87ad07fdef227796fe96ce5
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-01 21:02:51 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='myrino',
-    version='0.3',
+    version='1.0',
     author='amirali irvany',
     author_email='dev.metect@gmail.com',
     description='Myrino is an api-based library for Rubino messengers',
