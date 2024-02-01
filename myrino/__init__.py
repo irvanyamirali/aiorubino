c9dcd395fca257b72833ee7df62fccd04735d9d9
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-01 21:03:18 +0330

    Update __init__.py

--- myrino/__init__.py
+++ myrino/__init__.py
@@ -1,4 +1,6 @@
 from .client import Client
 
-__version__: int = 0.3
+__version__: int = 1.0
+__author__: str = 'amirali irvany'
 __github__: str = 'https://github.com/metect'
+__email__: str = 'dev.amirali.irvany@gmail.com'
