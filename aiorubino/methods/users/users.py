e3174d8127cd1081d7cdb06a4fc8aa4a97589c90
AmirAli Irvany <irvanyamirali@gmail.com>
2024-06-22 17:16:57 +0330

    Update users.py

--- aiorubino/methods/users/users.py
+++ aiorubino/methods/users/users.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from aiorubino.types import Results
 
 import aiorubino
+
+from typing import Optional
 
 
 class Users:
@@ -8,7 +10,7 @@ class Users:
     def __init__(self: "aiorubino.Client"):
         pass
 
-    async def get_my_profile_info(self, profile_id: Optional[str] = None) -> dict:
+    async def get_my_profile_info(self, profile_id: Optional[str] = None) -> Results:
         """
         Get your page information.
         If you want to access your page information, you don't need to enter the profile_id parameter.
