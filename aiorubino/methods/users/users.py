9440c158478eb7416b7d85f0608b873e27f18906
AmirAli Irvany <irvanyamirali@gmail.com>
2024-06-22 17:34:53 +0330

    Added `get_profile_highlights` method

--- aiorubino/methods/users/users.py
+++ aiorubino/methods/users/users.py
@@ -7,10 +7,7 @@ from typing import Optional
 
 class Users:
 
-    def __init__(self: "aiorubino.Client"):
-        pass
-
-    async def get_my_profile_info(self, profile_id: Optional[str] = None) -> Results:
+    async def get_my_profile_info(self: "aiorubino.Client", profile_id: Optional[str] = None) -> Results:
         """
         Get your page information.
         If you want to access your page information, you don't need to enter the profile_id parameter.
@@ -19,3 +16,14 @@ class Users:
             "profile_id": profile_id
         }
         return await self.api.execute(name="getMyProfileInfo", data=params)
+
+    async def get_profile_highlights(
+            self: "aiorubino.Client",
+            target_profile_id: str,
+            profile_id: Optional[str] = None
+    ) -> Results:
+        params: dict = {
+            "target_profile_id": target_profile_id,
+            "profile_id": profile_id
+        }
+        return await self.api.execute(name="getProfileHighlights", data=params)
